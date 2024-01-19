fd68c702e3ef530ef457963568ba8f53f31cfeb7
Diseyi <filomiinaa@gmail.com>
2024-01-19 12:40:11 +0100

    create social icon components

--- src/Tailwind.jsx
+++ src/Tailwind.jsx
@@ -244,17 +244,13 @@ export function Button() {
   );
 }
 
-export function Button_Plain() {
+export function Button_Large() {
   return (
-    <button
-      className="py-2 rounded hover:bg-sky-100 transition duration-300 ease-in-out px-6 font-semibold tracking-wider text-sky-800 text-sm"
-    >
-      Button
-    </button>
+    <button className="bg-sky-600 transition duration-150 ease-in-out hover:bg-sky-700 lg:text-xl lg:font-bold rounded text-white px-4 sm:px-10 py-2 sm:py-4 text-sm">Button</button>
   );
 }
 
-export function Button_Outline() {
+export function Button_Outlined() {
   return (
     <button
       className="py-2 rounded border border-sky-700 hover:border-sky-600 transition duration-300 ease-in-out px-6 font-semibold tracking-wider text-sky-800 text-sm"
@@ -343,7 +339,7 @@ export function Dialog() {
       </div>
       <div className="flex gap-2 shrink-0 flex-wrap items-center justify-end text-sky-slate-500">
         <button
-          className="py-2 rounded hover:bg-red-100 transition duration-300 ease-in-out px-6 font-semibold text-sm tracking-wider text-red-800"
+          className="py-2 rounded transition duration-300 ease-in-out px-6 font-semibold text-sm tracking-wider text-red-800"
         >
           Cancel
         </button>
@@ -509,15 +505,15 @@ export function Footer() {
       </div>
       <hr className="my-6 border-slate-200 sm:mx-auto lg:my-8" />
       <div className="sm:flex sm:items-center sm:justify-between">
-        <span className="text-sm text-slate-500 sm:text-center">
-          © 2022 LOGO. All Rights Reserved.
+        <span className="text-sm text-slate-600 sm:text-center">
+          © 2024 LOGO. All Rights Reserved.
         </span>
         <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
           <button
-            className="text-slate-500 hover:text-slate-900 "
+            className="text-slate-600 hover:text-slate-800 "
           >
             <svg
-              className="w-5 h-5"
+              className="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 24 24"
               aria-hidden="true"
@@ -530,10 +526,10 @@ export function Footer() {
             </svg>
           </button>
           <button
-            className="text-slate-500 hover:text-slate-900"
+            className="text-slate-600 hover:text-slate-800"
           >
             <svg
-              className="w-5 h-5"
+              className="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 24 24"
               aria-hidden="true"
@@ -546,10 +542,10 @@ export function Footer() {
             </svg>
           </button>
           <button
-            className="text-slate-500 hover:text-slate-900"
+            className="text-slate-600 hover:text-slate-800"
           >
             <svg
-              className="w-5 h-5"
+              className="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 24 24"
               aria-hidden="true"
@@ -558,10 +554,10 @@ export function Footer() {
             </svg>
           </button>
           <button
-            className="text-slate-500 hover:text-slate-900"
+            className="text-slate-600 hover:text-slate-800"
           >
             <svg
-              className="w-5 h-5"
+              className="w-6 h-6"
               fill="currentColor"
               viewBox="0 0 24 24"
               aria-hidden="true"
@@ -576,16 +572,6 @@ export function Footer() {
         </div>
       </div>
     </footer>
-  );
-}
-
-export function Icon() {
-  return (
-    <button
-      className='h-10 w-10 rounded-full flex items-center justify-center bg-sky-600 hover:bg-sky-700 transition duration-300 ease-in-out'
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" fill="white" data-name="Layer 1" viewBox="0 0 24 24" width="24" height="24"><path d="M12,0A12,12,0,1,0,24,12,12.013,12.013,0,0,0,12,0Zm0,22A10,10,0,1,1,22,12,10.011,10.011,0,0,1,12,22ZM7,9.5C7,8.119,7.672,7,8.5,7S10,8.119,10,9.5,9.328,12,8.5,12,7,10.881,7,9.5Zm7,0c0-1.381.672-2.5,1.5-2.5S17,8.119,17,9.5,16.328,12,15.5,12,14,10.881,14,9.5ZM12,15a21.923,21.923,0,0,0,6.007-1c-.615,2.28-2.664,5-6,5S6.615,16.28,6,14A21.841,21.841,0,0,0,12,15Z" /></svg>
-    </button>
   );
 }
 
@@ -688,11 +674,83 @@ export function Hero() {
           <p className="mt-5 sm:mt-10 lg:w-10/12 text-slate-500 font-normal text-center text-sm sm:text-lg">The seamless component building blocks for effortlessly crafting your perfect web application </p>
         </div>
         <div className="flex justify-center items-center">
-          <button className="bg-sky-700 transition duration-150 ease-in-out hover:bg-sky-600 lg:text-xl lg:font-bold rounded text-white px-4 sm:px-10 py-2 sm:py-4 text-sm">Get Started</button>
+          <button className="bg-sky-600 transition duration-150 ease-in-out hover:bg-sky-700 lg:text-xl lg:font-bold rounded text-white px-4 sm:px-10 py-2 sm:py-4 text-sm">Get Started</button>
           <button className="ml-4 bg-transparent transition duration-150 ease-in-out lg:text-xl lg:font-bold hover:text-sky-600 rounded border border-sky-700 hover:border-sky-600 text-sky-700 px-4 sm:px-10 py-2 sm:py-4 text-sm">Book Demo</button>
         </div>
       </div>
     </section>
+  );
+}
+
+export function Icon_Facebook() {
+  return (
+    <button className="text-slate-600 hover:text-slate-800">
+      <svg
+        className="w-6 h-6"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+  );
+}
+
+export function Icon_Instagram() {
+  return (
+    <button className="text-slate-600 hover:text-slate-800">
+      <svg
+        className="w-6 h-6"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+  );
+}
+
+export function Icon_X() {
+  return (
+    <button className="text-slate-600 hover:text-slate-800">
+      <svg
+        className="w-6 h-6"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+      >
+        <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
+      </svg>
+    </button>
+  );
+}
+
+export function Icon_Github() {
+  return (
+    <button className="text-slate-600 hover:text-slate-800">
+      <svg
+        className="w-6 h-6"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
   );
 }
 
@@ -710,7 +768,7 @@ export function Image() {
 
 export function Input() {
   return (
-    <input type="text" id="input" className="block w-full px-3 py-2 text-slate-900 border border-slate-300 rounded  focus:border-sky-500" />
+    <input type="text" id="input" className="block w-full px-3 py-2 text-slate-900 border border-slate-300 rounded outline-sky-600" />
   );
 }
 
@@ -952,7 +1010,7 @@ export function Search_Rounded() {
 
 export function Select() {
   return (
-    <select id="countries" className="bg-slate-50 border border-slate-300 outline-none text-slate-900 text-sm rounded-lg block w-full p-3">
+    <select id="countries" className="bg-slate-50 border border-slate-500 outline-none text-slate-900 text-sm rounded-lg block w-full p-3">
       <option selected>Choose your country</option>
       <option value="US">United States</option>
       <option value="CA">Canada</option>
